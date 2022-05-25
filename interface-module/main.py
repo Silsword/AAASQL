6a07096fe7c18b730afad75b2d7cfeb19d9d93de
Alexandra Kovrova <90903188+ShalottTheSorcerer@users.noreply.github.com>
2022-05-25 19:54:38 +0300

    Main fail upgraded

--- interface-module/main.py
+++ interface-module/main.py
@@ -5,6 +5,14 @@ from prettytable import PrettyTable
 def output(data):
     input_json = json.loads(data)
     table = PrettyTable()
+    max_length = 0
+    for i in input_json.keys():
+        if len(input_json[i]) > max_length:
+            max_length = len(input_json[i])
+    for i in input_json.keys():
+        if len(input_json[i]) < max_length:
+            while len(input_json[i]) != max_length:
+                input_json[i].append("-")
     for i in input_json.keys():
         table.add_column(i, input_json[i])
     print(table)
